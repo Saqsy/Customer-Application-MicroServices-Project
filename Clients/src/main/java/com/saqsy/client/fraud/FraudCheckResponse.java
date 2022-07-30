d92453b17093c6439fa7735403ef6b5a22b9b693
Saquib Kazi <Saquib.kazi99@gmail.com>
2022-07-30 18:40:08 +0530

    fix jackson library error

--- Clients/src/main/java/com/saqsy/client/fraud/FraudCheckResponse.java
+++ Clients/src/main/java/com/saqsy/client/fraud/FraudCheckResponse.java
@@ -3,9 +3,13 @@ package com.saqsy.client.fraud;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Getter
+@NoArgsConstructor
 public class FraudCheckResponse {
 
     Boolean isFraudster;
